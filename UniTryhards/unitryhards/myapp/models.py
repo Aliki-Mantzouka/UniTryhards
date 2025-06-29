c214fa1112d1bafb223a44c79eceb821d306896c
Aliki Mantzouka <alikimantzouka@gmail.com>
2025-06-29 15:02:00 +0300

    Added a characters limit (max 500) in the comments section

--- UniTryhards/unitryhards/myapp/models.py
+++ UniTryhards/unitryhards/myapp/models.py
@@ -51,7 +51,7 @@ class Paper(models.Model):
 class Comment(models.Model):
     paper = models.ForeignKey(Paper, on_delete=models.CASCADE, related_name="comments")  # Link to Paper model
     user = models.ForeignKey(User, on_delete=models.CASCADE)  # Link to User model (if users should be able to comment)
-    content = models.TextField()
+    content = models.TextField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
